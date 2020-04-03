aaeccedfa85fa7756745587bb86d4398eb7d279c
Gleisson <gleisson.matos@cubos.io>
2020-04-03 00:57:40 -0300

    feat: start control version

---
+++ index.js
@@ -0,0 +1,10 @@
+/*!
+* orbty
+* Copyright(c) 2020 Gleisson Mattos
+* http://github.com/gleissonmattos
+*
+* Licensed under the MIT license.
+* http://www.opensource.org/licenses/mit-license.php
+*/
+
+module.exports = require('./lib/orbty');
