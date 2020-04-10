07d811a07a01711aa9084db5bebaadb630d75dcc
Gleisson Mattos <contato.mattos@outlook.com>
2020-04-10 00:19:20 -0300

    test(helpers): add tests to getBasePath

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -1,4 +1,5 @@
 const { isOnlyNumber } = require("../lib/helpers/values");
+const { getBasePath } = require("../lib/helpers/url");
 
 describe("Helpers", () => {
 	describe("values", () => {
@@ -6,10 +7,22 @@ describe("Helpers", () => {
 			expect(isOnlyNumber("0123456789")).toBe(true);
 			expect(isOnlyNumber("012345a789")).toBe(false);
 			expect(isOnlyNumber("abcdfghyj")).toBe(false);
-			expect(isOnlyNumber("1")).toBe(true);
 			expect(isOnlyNumber("_1111a1")).toBe(false);
-			expect(isOnlyNumber("1111a1")).toBe(false);
 			expect(isOnlyNumber(" 55555 ")).toBe(false);
+			expect(isOnlyNumber("1111a1")).toBe(false);
+			expect(isOnlyNumber("1")).toBe(true);
+		});
+	});
+
+	describe("url", () => {
+		test("getBasePath: should get base path from url", () => {
+			expect(getBasePath("/posts/summer?day=1")).toBe("/posts");
+			expect(getBasePath("/user/posts")).toBe("/user");
+			expect(getBasePath("user/posts")).toBe("/user");
+			expect(getBasePath("/user")).toBe("/user");
+			expect(getBasePath("user")).toBe("/user");
+			expect(getBasePath("/")).toBe("/");
+			expect(getBasePath("")).toBe("/");
 		});
 	});
 });
