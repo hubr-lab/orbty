7f326a9499e47884dc7d2ac84232d93cd231082a
Gleisson Mattos <contato.mattos@outlook.com>
2020-04-11 18:10:53 -0300

    style: normalize status from 4 to 2

--- test/routers.test.js
+++ test/routers.test.js
@@ -1,22 +1,22 @@
 const Orbty = require("../index");
 
 describe("routers", () =>{
-    describe("create", () => {
+	describe("create", () => {
 
-        test("Should correct Orbty encaded instance", () => {
-            expect(
-                ((new Orbty()).create(null, "/", () => {})) instanceof Orbty
-            ).toBeTruthy();
-        });
+		test("Should correct Orbty encaded instance", () => {
+			expect(
+				((new Orbty()).create(null, "/", () => {})) instanceof Orbty
+			).toBeTruthy();
+		});
 
-        test("Should error case path cannot be string or RegExp", () => {
-            const orbty = new Orbty();
+		test("Should error case path cannot be string or RegExp", () => {
+			const orbty = new Orbty();
 
-            expect(orbty.create.bind(orbty, null, [], () => {})).toThrowError(Error);
-            expect(orbty.create.bind(orbty, null, Error, () => {})).toThrowError(Error);
-            expect(orbty.create.bind(orbty, null, 11, () => {})).toThrowError(Error);
-            expect(orbty.create.bind(orbty, null, {}, () => {})).toThrowError(Error);
-            expect(orbty.create.bind(orbty, null, () => {}, () => {})).toThrowError(Error);
-        });
-    });
+			expect(orbty.create.bind(orbty, null, [], () => {})).toThrowError(Error);
+			expect(orbty.create.bind(orbty, null, Error, () => {})).toThrowError(Error);
+			expect(orbty.create.bind(orbty, null, 11, () => {})).toThrowError(Error);
+			expect(orbty.create.bind(orbty, null, {}, () => {})).toThrowError(Error);
+			expect(orbty.create.bind(orbty, null, () => {}, () => {})).toThrowError(Error);
+		});
+	});
 })
