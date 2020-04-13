b3b466ccc05e1857a383b6592bc13c6142c0eaee
Gleisson Mattos <contato.mattos@outlook.com>
2020-04-13 02:10:55 -0300

    refactor: separe match super method

---
+++ test/instance-router.test.js
@@ -0,0 +1,23 @@
+const Orbty = require("../index");
+
+describe("Orbty router instance", () => {
+	test("Should return orbty instance", () => {
+		const instance = new Orbty();
+
+		expect(typeof instance.get).toBe("function");
+		expect(typeof instance.use).toBe("function");
+		expect(typeof instance.match).toBe("function");
+	});
+
+	test("Should use orbty with another orbty instance", () => {
+		const instance = new Orbty();
+		const anotherInstance = new Orbty();
+
+		anotherInstance.get("/world", () => "OK");
+		instance.use("/hello", anotherInstance);
+
+		const match = instance.match("GET", "/hello/world");
+		expect(match).not.toBeNull();
+		expect(match instanceof Orbty).toBeTruthy();
+	});
+});
