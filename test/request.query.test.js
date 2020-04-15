161823fd3b922ebc7cd25a136597c608cfe94a2a
Gleisson Mattos <contato.mattos@outlook.com>
2020-04-15 19:06:36 -0300

    test: url query parser

---
+++ test/request.query.test.js
@@ -0,0 +1,42 @@
+const Orbty = require("..");
+const request = require("supertest");
+const faker = require("faker");
+
+describe("Request query parser", () => {
+	test("Should response parsed url query", (done) => {
+		const orbty = new Orbty();
+		const name = faker.name.firstName();
+		const number = faker.random.number(10);
+
+		orbty.get("/foo", ({ query }) => {
+			return query;
+		});
+
+		request(orbty.server())
+			.get(
+				`/foo?name=${name}&num=${number}`
+			).expect(200, (err, { body }) => {
+				expect(err).toBeNull();
+				expect(body.name).toBe(name);
+				expect(body.num).toBe(String(number));
+				done();
+			});
+	});
+
+	test("Should response empty query object", (done) => {
+		const orbty = new Orbty();
+
+		orbty.get("/foo", ({ query }) => {
+			return query;
+		});
+
+		request(orbty.server())
+			.get(
+					"/foo"
+			).expect(200, (err, { body }) => {
+					expect(err).toBeNull();
+					expect(body).toMatchObject({});
+					done();
+			});
+	});
+});
